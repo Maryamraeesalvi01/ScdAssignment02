c2c93fb3d06d102493eaa45c2a4c1513ab7c647e
Maryamraeesalvi01 <maryamalvi133@gmail.com>
2023-10-12 20:54:22 -0700

    Question 2 Assignment 02 First NonRepeating Charachter

--- NonRepeatingStreamMain.java
+++ NonRepeatingStreamMain.java
@@ -1,10 +1,12 @@
 package ScdAssignment02;
 import java.util.*;
+// Main for Checking First Non Repeating Value 
 public class NonRepeatingStreamMain 
 {
     public static void main(String[]args)
     {
         First_NonRepeating_Stream s = new First_NonRepeating_Stream();
+        // adding value in the stream
         s.add('a');
         s.add('b');
         s.add('a');
@@ -16,7 +18,9 @@ public class NonRepeatingStreamMain
 }
 class First_NonRepeating_Stream
 {
+  // List of stream
   private LinkedList <Character> cList;
+  // Hashmap to get count of every charachter 
   private HashMap<Character,Integer> check;
   public First_NonRepeating_Stream()
   {
@@ -26,12 +30,15 @@ class First_NonRepeating_Stream
   public void add(char c )
   {
     cList.addLast(c);
+    // charachter as a key and value as count 
     check.put(c, check.getOrDefault(c, 0)+1);
   }
+  // checking nonrepeating integer 
   public char getFirstNonRepeating()
   {
     while(!cList.isEmpty())
     {
+      // takes first charachter in the list 
         char firstnonrepeating=cList.getFirst();
         if(check.get(firstnonrepeating)==1)
         {
